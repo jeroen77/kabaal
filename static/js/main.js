1e081963e3ed339b7f47028b5309ba44fb624a7b
jeroen77 <jrondeel@tripolis.com>
2014-05-05 10:22:25 +0200

    adds carousel indicators and caption text

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.kabaal-carousel').slick({
         autoplay: true,
-        autoplaySpeed: 5000
+        autoplaySpeed: 5000,
+        dots: true
     });
 });
